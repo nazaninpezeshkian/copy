a15df8da4835aa4f674d98cd68c7e2466f7a9d88
nazaninpezeshkian <nazanin.pezeshkian@yahoo.com>
2023-03-05 19:11:13 -0800

    update app.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
    <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/careers" element={<Careers />} />
+     
       </Routes>
     </div>
   );
